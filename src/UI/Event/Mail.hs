6c72f36ae58e5f0325f69eedc5970a056314d023
Roman Joost <roman@bromeco.de>
2017-07-23 15:14:33 +1000

    mailview: Allow to scroll through mail
    While viewing e-mail, allow the user to select other mail in the inbox
    to view.

--- src/UI/Event/Mail.hs
+++ src/UI/Event/Mail.hs
@@ -1,13 +1,16 @@
 module UI.Event.Mail where
 
-import qualified Brick.Main   as M
-import qualified Brick.Types  as T
-import qualified Graphics.Vty as V
+import qualified Brick.Main         as M
+import qualified Brick.Types        as T
+import qualified Brick.Widgets.List as L
+import qualified Graphics.Vty       as V
 import           UI.Types
 
+-- | The mail view shows a shortened list of mails. Forward all key strokes to
+-- the list of mails by default.
 mailEvent :: AppState -> T.BrickEvent () e -> T.EventM () (T.Next AppState)
 mailEvent (AppState s l ViewMail) (T.VtyEvent e) =
   case e of
     V.EvKey V.KEsc [] -> M.continue $ AppState s l Main
-    _ -> M.continue $ AppState s l ViewMail
+    ev -> L.handleListEvent ev l >>= \l' -> M.continue $ AppState s l' ViewMail
 mailEvent l _ = M.continue l
