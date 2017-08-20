purebred-mua/purebred

commit dc74b5cdc16dde922c77b8856a0061c55987f539

Make database option optional
This was the default behaviour and should remain the default behaviour:
by default we open the database returned by notmuch.

app/Main.hs
@@ -7,18 +7,27 @@ import qualified Brick.Main as M
 import Control.Monad (void)
 import Storage.Notmuch (getDatabasePath)
 import Config.Main (defaultConfig)
+import Data.Maybe (fromMaybe)
 import Options.Applicative hiding (str)
+import qualified Options.Applicative.Builder as Builder
 import Data.Semigroup ((<>))
 
-data AppConfig = AppConfig { databaseFilepath :: String }
+data AppConfig = AppConfig { databaseFilepath :: Maybe String }
 
 appconfig :: Parser AppConfig
-appconfig = AppConfig <$> strOption ( long "database" <> metavar "DATABASE" <> help "Filepath to notmuch database" )
+appconfig =
+    AppConfig <$>
+    (optional $
+     Builder.option
+         Builder.str
+         (long "database" <> metavar "DATABASE" <>
+          help "Filepath to notmuch database"))
 
 main :: IO ()
 main = do
     cfg <- execParser opts
-    s <- initialState =<< defaultConfig =<< pure (databaseFilepath cfg)
+    dbpath <- getDatabasePath
+    s <- initialState =<< defaultConfig =<< pure (fromMaybe dbpath (databaseFilepath cfg))
     void $ M.defaultMain (theApp s) s
         where
             opts = info (appconfig <**> helper)

commit c6d10f68469f8271303bf2b4e7b4d93d3a3dac31

Do not always evaluate default database path
This patch fixes a regression introduced in
dc74b5cdc16dde922c77b8856a0061c55987f539 due to evaluating the notmuch
database path on startup by default. It resulted in an error:

    Error reading configuration file /home/travis/.notmuch-config: No such file or directory

What I intended tho was to keep precedence over the command line option,
while the fall back is the database from the default notmuch config.

app/Main.hs
@@ -7,7 +7,6 @@ import qualified Brick.Main as M
 import Control.Monad (void)
 import Storage.Notmuch (getDatabasePath)
 import Config.Main (defaultConfig)
-import Data.Maybe (fromMaybe)
 import Options.Applicative hiding (str)
 import qualified Options.Applicative.Builder as Builder
 import Data.Semigroup ((<>))
@@ -26,8 +25,10 @@ appconfig =
 main :: IO ()
 main = do
     cfg <- execParser opts
-    dbpath <- getDatabasePath
-    s <- initialState =<< defaultConfig =<< pure (fromMaybe dbpath (databaseFilepath cfg))
+    dbfp <- case databaseFilepath cfg of
+          Nothing -> getDatabasePath
+          Just fp -> pure fp
+    s <- initialState =<< defaultConfig dbfp
     void $ M.defaultMain (theApp s) s
         where
             opts = info (appconfig <**> helper)