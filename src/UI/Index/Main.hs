purebred-mua/purebred

commit 0069c0280c5106113d101c998b7cd23f098974ec

Track different main modes in app state
This is the basis to re-factor the key bindings, since we want create a
mapping between application mode and keybindings. There is no real point
of tracking different modes for a single view in a separate sum type.

Issue: #45

src/UI/Index/Main.hs
@@ -30,12 +30,12 @@ import Config.Main
 drawMain :: AppState -> [Widget Name]
 drawMain s = [ui]
   where
-    editorFocus = view (asMailIndex . miMode) s == SearchMail
+    editorFocus = view asAppMode s == SearchMail
     inputBox = E.renderEditor editorDrawContent editorFocus (view (asMailIndex . miSearchEditor) s)
     ui = vBox [renderMailList s, statusbar s, vLimit 1 inputBox]
 
 renderMailList :: AppState -> Widget Name
-renderMailList s = let listFocus = view (asMailIndex . miMode) s == BrowseMail
+renderMailList s = let listFocus = view asAppMode s == BrowseMail
                    in L.renderList (listDrawElement s) listFocus (view (asMailIndex . miListOfMails) s)
 
 listDrawElement :: AppState -> Bool -> NotmuchMail -> Widget Name
@@ -69,17 +69,17 @@ renderMailTagsWidget m ignored =
 -- list, the other is to allow the user to easily change the list.
 mainEvent :: AppState -> T.BrickEvent Name e -> T.EventM Name (T.Next AppState)
 mainEvent s e =
-    case view (asMailIndex . miMode) s of
-        BrowseMail ->
-            handleEvent
-                (view (asConfig . confIndexView . ivKeybindings) s)
-                listEventDefault
-                s
-                e
+    case view asAppMode s of
         SearchMail ->
             handleEvent
                 (view (asConfig . confIndexView . ivSearchKeybindings) s)
                 searchInputEventDefault
+                s
+                e
+        _ ->
+            handleEvent
+                (view (asConfig . confIndexView . ivKeybindings) s)
+                listEventDefault
                 s
                 e
 
@@ -95,7 +95,7 @@ listEventDefault s e =
     L.handleListEvent e (view (asMailIndex . miListOfMails) s) >>=
     \mi' ->
          M.continue $
-         set (asMailIndex . miListOfMails) mi' s & set asAppMode Main &
+         set (asMailIndex . miListOfMails) mi' s & set asAppMode BrowseMail &
          set asError Nothing
 
 
@@ -107,4 +107,4 @@ searchInputEventDefault s ev =
     E.handleEditorEvent ev (view (asMailIndex . miSearchEditor) s) >>=
     \ed ->
          M.continue $
-         set (asMailIndex . miSearchEditor) ed s & set asAppMode Main
+         set (asMailIndex . miSearchEditor) ed s & set asAppMode BrowseMail

src/UI/GatherHeaders/Main.hs
@@ -88,5 +88,5 @@ invokeEditor s = do
   tmpfile <- emptySystemTempFile "purebred.tmp"
   status <- system (editor <> " " <> tmpfile)
   case status of
-    ExitFailure _ -> pure $ set asAppMode Main s -- ^ show error XXX
+    ExitFailure _ -> pure $ set asAppMode BrowseMail s -- ^ show error XXX
     ExitSuccess -> pure $ set asAppMode ComposeEditor s & asCompose . cTmpFile ?~ tmpfile -- ^ go to compose editor

src/Types.hs
@@ -20,10 +20,12 @@ import Error
 
 -- | The global application mode
 data Mode
-    = Main  -- ^ focus is on the main screen
+    = BrowseMail  -- ^ input focus goes to navigating the list of mails (main screen)
+    | SearchMail  -- ^ input focus goes to manipulating the notmuch search (main screen)
     | ViewMail  -- ^ focus is on the screen showing the entire mail
     | GatherHeaders  -- ^ focus is on the command line to gather input for composing an e-mail
     | ComposeEditor  -- ^ edit the final e-mail
+    deriving (Eq,Show)
 
 -- | Used to identify widgets in brick
 data Name =
@@ -35,12 +37,6 @@ data Name =
     | GatherHeadersSubject
     deriving (Eq,Show,Ord)
 
--- | Modes for the main window to distinguish focus
-data MainMode
-    = BrowseMail   -- ^ input focus goes to navigating the list of mails
-    | SearchMail   -- ^ input focus goes to manipulating the notmuch search
-    deriving (Eq)
-
 {- | main application interface
 
 The main UI shows a list of e-mails, allows the user to manipulate the notmuch
@@ -50,18 +46,13 @@ search and composes e-mails from here.
 data MailIndex = MailIndex
     { _miListOfMails  :: L.List Name NotmuchMail
     , _miSearchEditor :: E.Editor T.Text Name
-    , _miMode         :: MainMode
     }
 
 miListOfMails :: Lens' MailIndex (L.List Name NotmuchMail)
-miListOfMails f (MailIndex a b c) = fmap (\a' -> MailIndex a' b c) (f a)
+miListOfMails f (MailIndex a b) = fmap (\a' -> MailIndex a' b) (f a)
 
 miSearchEditor :: Lens' MailIndex (E.Editor T.Text Name)
-miSearchEditor f (MailIndex a b c) = fmap (\b' -> MailIndex a b' c) (f b)
-
-miMode :: Lens' MailIndex MainMode
-miMode f (MailIndex a b c) = fmap (\c' -> MailIndex a b c') (f c)
-
+miSearchEditor f (MailIndex a b) = fmap (\b' -> MailIndex a b') (f b)
 
 data HeadersState = ShowAll | Filtered
 

src/UI/App.hs
@@ -22,7 +22,8 @@ import Types
 drawUI :: AppState -> [Widget Name]
 drawUI s =
     case view asAppMode s of
-        Main -> drawMain s
+        BrowseMail -> drawMain s
+        SearchMail -> drawMain s
         ViewMail -> drawMail s
         GatherHeaders -> drawInteractiveHeaders s
         ComposeEditor -> drawComposeEditor s
@@ -30,7 +31,8 @@ drawUI s =
 appEvent :: AppState -> T.BrickEvent Name e -> T.EventM Name (T.Next AppState)
 appEvent s e =
     case view asAppMode s of
-        Main -> mainEvent s e
+        BrowseMail -> mainEvent s e
+        SearchMail -> mainEvent s e
         ViewMail -> mailEvent s e
         GatherHeaders -> interactiveGatherHeaders s e
         ComposeEditor -> composeEditor s e
@@ -49,9 +51,8 @@ initialState conf = do
                      EditorInput
                      Nothing
                      searchterms)
-                BrowseMail
           mv = MailView Nothing Filtered
-        in pure $ AppState conf mi mv initialCompose Main Nothing
+        in pure $ AppState conf mi mv initialCompose BrowseMail Nothing
 
 theApp :: AppState -> M.App AppState e Name
 theApp s =

src/UI/ComposeEditor/Keybindings.hs
@@ -49,7 +49,7 @@ sendMail s = do
                 (unlines $ E.getEditContents $ view (asCompose . cSubject) s)
                 body
     liftIO $ renderSendMail m
-    M.continue $ set asCompose initialCompose s & set asAppMode Main
+    M.continue $ set asCompose initialCompose s & set asAppMode BrowseMail
 
 resetCompose :: AppState -> AppState
-resetCompose s = set asCompose initialCompose s & set asAppMode Main
+resetCompose s = set asCompose initialCompose s & set asAppMode BrowseMail

src/UI/Index/Keybindings.hs
@@ -46,7 +46,7 @@ indexsearchKeybindings =
 
 focusSearch :: AppState -> T.EventM Name (T.Next AppState)
 focusSearch = continue
-                . set (asMailIndex . miMode) SearchMail
+                . set asAppMode SearchMail
                 . over (asMailIndex . miSearchEditor) (E.applyEdit gotoEOL)
 
 displayMail :: AppState -> T.EventM Name (T.Next AppState)
@@ -73,7 +73,7 @@ updateStateWithParsedMail :: AppState -> IO AppState
 updateStateWithParsedMail s = ($ s) <$>
     case L.listSelectedElement (view (asMailIndex . miListOfMails) s) of
         Just (_,m) -> either
-            (\e -> setError e . set asAppMode Main)
+            (\e -> setError e . set asAppMode BrowseMail)
             (\pmail -> set (asMailView . mvMail) (Just pmail) . set asAppMode ViewMail)
             <$> runExceptT (parseMail m (view (asConfig . confNotmuch . nmDatabase) s))
         Nothing -> pure id
@@ -103,7 +103,7 @@ replyMail s =
                    <$> runExceptT (parseMail m (view (asConfig . confNotmuch . nmDatabase) s))
     Nothing -> pure id
   where
-    handleErr e = set asAppMode Main . setError e
+    handleErr e = set asAppMode BrowseMail . setError e
     handleMail pmail =
       set (asCompose . cTo) (E.editor GatherHeadersTo Nothing $ getFrom pmail)
       . set (asCompose . cFrom) (E.editor GatherHeadersFrom Nothing $ getTo pmail)
@@ -119,7 +119,7 @@ toggleComposeEditorAndMain s =
         Nothing -> continue s
 
 cancelSearch  :: AppState -> T.EventM Name (T.Next AppState)
-cancelSearch s = continue $ set (asMailIndex . miMode) BrowseMail s
+cancelSearch s = continue $ set asAppMode BrowseMail s
 
 setError :: Error -> AppState -> AppState
 setError = set asError . Just
@@ -133,5 +133,4 @@ applySearchTerms s =
 reloadListOfMails :: Vector NotmuchMail -> AppState -> AppState
 reloadListOfMails vec =
   set (asMailIndex . miListOfMails) (L.list ListOfMails vec 1)
-  . set asAppMode Main
-  . set (asMailIndex . miMode) BrowseMail
+  . set asAppMode BrowseMail

src/UI/Keybindings.hs
@@ -29,7 +29,7 @@ lookupKeybinding :: Event -> [Keybinding] -> Maybe Keybinding
 lookupKeybinding e = find (\x -> view kbEvent x == e)
 
 cancelToMain :: AppState -> T.EventM Name (T.Next AppState)
-cancelToMain s = M.continue $ set asAppMode Main s
+cancelToMain s = M.continue $ set asAppMode BrowseMail s
 
 initialCompose :: Compose
 initialCompose =

src/UI/Mail/Keybindings.hs
@@ -12,7 +12,7 @@ import Types
 displayMailKeybindings :: [Keybinding]
 displayMailKeybindings =
   [ Keybinding "Return to list of mails"
-    (V.EvKey V.KEsc []) (\s -> M.continue $ set asAppMode Main s)
+    (V.EvKey V.KEsc []) (\s -> M.continue $ set asAppMode BrowseMail s)
   , Keybinding "Scroll e-mail up" (V.EvKey V.KBS []) (\s -> scrollMailViewPage s T.Up)
   , Keybinding "Scroll e-mail down" (V.EvKey (V.KChar ' ') []) (\s -> scrollMailViewPage s T.Down)
   , Keybinding "toggle between filtered and all Headers" (V.EvKey (V.KChar 'h') []) (M.continue . toggleHeaders)

commit cf83d8f774e02b97fbf7fb45f7cfa7a75c344ddc

Fix regression when manipulating tags
This fixes a regression introduced with
0069c0280c5106113d101c998b7cd23f098974ec since it would change the focus
back to the mail index upon editing the tags.

src/UI/Index/Main.hs
@@ -107,4 +107,4 @@ searchInputEventDefault s ev =
     E.handleEditorEvent ev (view (asMailIndex . miSearchEditor) s) >>=
     \ed ->
          M.continue $
-         set (asMailIndex . miSearchEditor) ed s & set asAppMode BrowseMail
+         set (asMailIndex . miSearchEditor) ed s