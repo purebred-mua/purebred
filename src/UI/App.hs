070d35c05b9529f93ced2a2b244bd76f7e0d0699
Róman Joost <roman@bromeco.de>
2017-10-27 16:36:35 +1000

    WIP refactoring the event handler
    This is abstracting the event handling over the Mode similar to what
    we've done with the Actions.

--- src/UI/App.hs
+++ src/UI/App.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
 -- | The main application module
 module UI.App where
 
@@ -6,18 +8,21 @@ import Brick.Types (Widget)
 import qualified Brick.Types as T
 import qualified Brick.Widgets.Edit as E
 import qualified Brick.Widgets.List as L
+import Graphics.Vty.Input.Events (Event)
 import Control.Lens.Getter (view)
 import Control.Monad.Except (runExceptT)
 import System.Exit (die)
 
 import Storage.Notmuch (getMessages)
+import UI.Keybindings (handleEvent)
 import UI.ComposeEditor.Main (composeEditor, drawComposeEditor)
 import UI.GatherHeaders.Main
        (drawInteractiveHeaders, interactiveGatherHeaders)
-import UI.Index.Main (drawMain, mainEvent)
+import UI.Index.Main (drawMain, mainEvent, listEventDefault)
 import UI.Actions (initialCompose)
 import UI.Mail.Main (drawMail, mailEvent)
 import UI.Help.Main (drawHelp, handleHelpEvents)
+import Data.Proxy
 import Types
 
 drawUI :: AppState -> [Widget Name]
@@ -31,8 +36,34 @@ drawUI s =
         ComposeEditor -> drawComposeEditor s
         Help -> drawHelp s
 
+class EventHandler (m :: Mode) where
+  handle :: Proxy m -> AppState -> Event -> T.EventM Name (T.Next AppState)
+  fallback :: Proxy m -> AppState -> Event -> T.EventM Name (T.Next AppState)
+
+instance EventHandler 'BrowseMail where
+  handle _ s e = handleEvent (view (asConfig . confIndexView . ivKeybindings) s) listEventDefault s e
+
+instance EventHandler 'SearchMail where
+  handle _ s e = handleEventLensed
+
+instance EventHandler 'ManageTags where
+  handle _ s e = mainEvent s e
+
+dispatch :: EventHandler m => Proxy m -> AppState -> T.BrickEvent Name e -> T.EventM Name (T.Next AppState)
+dispatch m s (T.VtyEvent e) = handle m s e
+
 appEvent :: AppState -> T.BrickEvent Name e -> T.EventM Name (T.Next AppState)
-appEvent s ev = case ev of
+appEvent s (T.VtyEvent ev) =
+  case view asAppMode s of
+    BrowseMail -> handle (Proxy :: Proxy 'BrowseMail) s ev
+    SearchMail -> handle (Proxy :: Proxy 'SearchMail) s ev
+    ManageTags -> handle (Proxy :: Proxy 'ManageTags) s ev
+    ViewMail -> handle (Proxy :: Proxy 'ViewMail) s ev
+    GatherHeaders -> handle (Proxy :: Proxy 'GatherHeaders) s ev
+    ComposeEditor -> handle (Proxy :: Proxy 'ComposeEditor) s ev
+    Help -> handle (Proxy :: Proxy 'Help) s ev
+{-
+case ev of
   T.VtyEvent e ->
     case view asAppMode s of
         BrowseMail -> mainEvent s e
@@ -43,6 +74,7 @@ appEvent s ev = case ev of
         ComposeEditor -> composeEditor s e
         Help -> handleHelpEvents s e
   _ -> M.continue s  -- we only handle Vty events
+-}
 
 initialState :: InternalConfiguration -> IO AppState
 initialState conf = do
