3b13ce80a22410bf07bdd6f9650239975b410077
Roman Joost <roman@bromeco.de>
2017-12-25 21:41:01 +1000

    Make the "mutt"-like keybindings the default
    The chance that users know mutt keybindings are higher than our current
    default. Therefore, use mutt like keybindings are the default until we
    have a need to separate them with different configs.

--- src/UI/Mail/Keybindings.hs
+++ src/UI/Mail/Keybindings.hs
@@ -12,6 +12,7 @@ import Types
 displayMailKeybindings :: [Keybinding 'ViewMail (Brick.Next AppState)]
 displayMailKeybindings =
     [ Keybinding (V.EvKey V.KEsc []) (noop `chain'` (focus :: Action 'BrowseMail AppState) `chain` continue)
+    , Keybinding (V.EvKey (V.KChar 'q') []) (noop `chain'` (focus :: Action 'BrowseMail AppState) `chain` continue)
     , Keybinding (V.EvKey V.KBS []) (scrollUp `chain` continue)
     , Keybinding (V.EvKey (V.KChar ' ') []) (scrollDown `chain` continue)
     , Keybinding (V.EvKey (V.KChar 'h') []) (toggleHeaders `chain` continue)
@@ -22,4 +23,6 @@ displayIndexKeybindings :: [Keybinding 'BrowseMail (Brick.Next AppState)]
 displayIndexKeybindings =
     [ Keybinding (V.EvKey V.KDown []) (listDown `chain` displayMail `chain` continue)
     , Keybinding (V.EvKey V.KUp []) (listUp `chain` displayMail `chain` continue)
+    , Keybinding (V.EvKey (V.KChar 'j') []) (listDown `chain` displayMail `chain` continue)
+    , Keybinding (V.EvKey (V.KChar 'k') []) (listUp `chain` displayMail `chain` continue)
     ]
