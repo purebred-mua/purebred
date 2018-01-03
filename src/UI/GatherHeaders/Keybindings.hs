919f6cee59e397c963bd76af4ee05c0493973acc
Roman Joost <roman@bromeco.de>
2018-01-03 16:07:49 +1000

    Use keybindings to choose which mode to change to
    Instead of switching modes upon completing each GatherHeaders mode, use
    the declarative way to encode in the keybindings to which mode we're
    switching to. This is more flexible for the user and allows to re-use
    the modes for later editing in the compose editor.

--- src/UI/GatherHeaders/Keybindings.hs
+++ src/UI/GatherHeaders/Keybindings.hs
@@ -8,18 +8,18 @@ import UI.Actions
 
 interactiveGatherHeadersKeybindings :: [Keybinding 'GatherHeadersFrom (T.Next AppState)]
 interactiveGatherHeadersKeybindings =
-    [Keybinding (V.EvKey V.KEsc []) (noop `chain'` (focus :: Action 'BrowseThreads AppState) `chain` continue)
-    , Keybinding (V.EvKey V.KEnter []) (done `chain`continue)
+    [ Keybinding (V.EvKey V.KEsc []) (noop `chain'` (focus :: Action 'BrowseThreads AppState) `chain` continue)
+    , Keybinding (V.EvKey V.KEnter []) (noop `chain'` (focus :: Action 'GatherHeadersTo AppState) `chain` continue)
     ]
 
 interactiveGatherHeadersToKeybindings :: [Keybinding 'GatherHeadersTo (T.Next AppState)]
 interactiveGatherHeadersToKeybindings =
-    [Keybinding (V.EvKey V.KEsc []) (noop `chain'` (focus :: Action 'BrowseThreads AppState) `chain` continue)
-    , Keybinding (V.EvKey V.KEnter []) (done `chain`continue)
+    [ Keybinding (V.EvKey V.KEsc []) (noop `chain'` (focus :: Action 'BrowseThreads AppState) `chain` continue)
+    , Keybinding (V.EvKey V.KEnter []) (noop `chain'` (focus :: Action 'GatherHeadersSubject AppState) `chain` continue)
     ]
 
 interactiveGatherHeadersSubjectKeybindings :: [Keybinding 'GatherHeadersSubject (T.Next AppState)]
 interactiveGatherHeadersSubjectKeybindings =
-    [Keybinding (V.EvKey V.KEsc []) (noop `chain'` (focus :: Action 'BrowseThreads AppState) `chain` continue)
+    [ Keybinding (V.EvKey V.KEsc []) (noop `chain'` (focus :: Action 'BrowseThreads AppState) `chain` continue)
     , Keybinding (V.EvKey V.KEnter []) invokeEditor
     ]
