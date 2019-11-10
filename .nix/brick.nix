54aaa3b4552495f769ff2a07532e22feecc4a306
Róman Joost <roman@bromeco.de>
2019-11-10 14:41:35 +1000

    Use focusRing to query for the amount of matches
    Use a new focus ring function in order to determine how many total
    matches we are displaying.
    
    Relates to https://github.com/purebred-mua/purebred/issues/279

--- .nix/brick.nix
+++ .nix/brick.nix
@@ -1,20 +1,20 @@
-{ mkDerivation, base, config-ini, containers, contravariant
-, data-clist, deepseq, directory, dlist, filepath, microlens
-, microlens-mtl, microlens-th, QuickCheck, stdenv, stm
+{ mkDerivation, base, bytestring, config-ini, containers
+, contravariant, data-clist, deepseq, directory, dlist, filepath
+, microlens, microlens-mtl, microlens-th, QuickCheck, stdenv, stm
 , template-haskell, text, text-zipper, transformers, unix, vector
 , vty, word-wrap
 }:
 mkDerivation {
   pname = "brick";
-  version = "0.47";
-  sha256 = "47ce722f7a5f0457c97222823e863e455fe8b30710c2a9926d5930a9703892be";
+  version = "0.50.1";
+  sha256 = "fe9c6e3fcd71fa4a97bf12411256970dc10a3174623c95386e0e77a2d74d6673";
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    base config-ini containers contravariant data-clist deepseq
-    directory dlist filepath microlens microlens-mtl microlens-th stm
-    template-haskell text text-zipper transformers unix vector vty
-    word-wrap
+    base bytestring config-ini containers contravariant data-clist
+    deepseq directory dlist filepath microlens microlens-mtl
+    microlens-th stm template-haskell text text-zipper transformers
+    unix vector vty word-wrap
   ];
   testHaskellDepends = [
     base containers microlens QuickCheck vector
