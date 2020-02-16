8e4110e17303026a8f123b24bdbd2470bc8a4b70
Róman Joost <roman@bromeco.de>
2020-02-16 11:05:09 +1000

    Use brick 0.52 in nix too

--- .nix/brick.nix
+++ .nix/brick.nix
@@ -1,18 +1,18 @@
 { mkDerivation, base, bytestring, config-ini, containers
-, contravariant, data-clist, deepseq, directory, dlist, filepath
-, microlens, microlens-mtl, microlens-th, QuickCheck, stdenv, stm
-, template-haskell, text, text-zipper, transformers, unix, vector
-, vty, word-wrap
+, contravariant, data-clist, deepseq, directory, dlist, exceptions
+, filepath, microlens, microlens-mtl, microlens-th, QuickCheck
+, stdenv, stm, template-haskell, text, text-zipper, transformers
+, unix, vector, vty, word-wrap
 }:
 mkDerivation {
   pname = "brick";
-  version = "0.50.1";
-  sha256 = "fe9c6e3fcd71fa4a97bf12411256970dc10a3174623c95386e0e77a2d74d6673";
+  version = "0.52";
+  sha256 = "368dfdbb054fa4b14c091895f1b85742559d6c134081b1546ba3c75ab125e380";
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
     base bytestring config-ini containers contravariant data-clist
-    deepseq directory dlist filepath microlens microlens-mtl
+    deepseq directory dlist exceptions filepath microlens microlens-mtl
     microlens-th stm template-haskell text text-zipper transformers
     unix vector vty word-wrap
   ];
