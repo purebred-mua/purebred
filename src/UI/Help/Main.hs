928ba867c0011725db6cace4446205bad40769fb
Róman Joost <roman@bromeco.de>
2020-04-23 18:27:05 +1000

    Use a tool to dump keybindings
    This tool can be used to dump our key bindings as Asciidoc markup. The
    markup can be used to be incorporated in our documentation.
    
    Reason as to why this would be useful: part of customising our
    keybindings is knowing the existing keybindings and what key event they
    are registered for. This way, there is no need to dig through the code
    to figure out what is registered for what event.

--- src/UI/Help/Main.hs
+++ src/UI/Help/Main.hs
@@ -14,7 +14,12 @@
 -- You should have received a copy of the GNU Affero General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 {-# LANGUAGE OverloadedStrings #-}
-module UI.Help.Main (renderHelp) where
+module UI.Help.Main
+  ( renderHelp
+  , createKeybindingIndex
+  , HelpIndex
+  , KeybindingHelp(..)
+  ) where
 
 import Data.Function (on)
 import Data.List (nubBy)
@@ -22,38 +27,77 @@ import Brick.Types (Padding(..), Widget)
 import qualified Brick.Types as T
 import Brick.Widgets.Core
        (viewport, hLimit, padLeft, padBottom, padRight, txt, (<=>),
-        (<+>), vBox, withAttr)
+        (<+>), vBox, withAttr, emptyWidget)
 import Graphics.Vty.Input.Events (Event(..), Key(..), Modifier(..))
-import Control.Lens (view, views)
+import Control.Lens (view, views, ifoldr)
+import qualified Data.Map.Strict as M
 import Data.Text (Text, singleton, intercalate, pack)
 import Config.Main (helpTitleAttr, helpKeybindingAttr)
 import Types
 import UI.Utils (titleize, Titleize)
 
-renderHelp :: AppState -> Widget Name
-renderHelp s = viewport ScrollingHelpView T.Vertical $ vBox
-  [ views (asConfig . confIndexView . ivBrowseThreadsKeybindings) (renderKbGroup ListOfThreads) s
-  , views (asConfig . confIndexView . ivSearchThreadsKeybindings) (renderKbGroup SearchThreadsEditor) s
-  , views (asConfig . confIndexView . ivManageThreadTagsKeybindings) (renderKbGroup ManageThreadTagsEditor) s
-  , views (asConfig . confMailView . mvKeybindings) (renderKbGroup ScrollingMailView) s
-  , views (asConfig . confHelpView . hvKeybindings) (renderKbGroup ScrollingHelpView) s
-  , views (asConfig . confComposeView . cvListOfAttachmentsKeybindings) (renderKbGroup ComposeListOfAttachments) s
-  , views (asConfig . confFileBrowserView . fbKeybindings) (renderKbGroup ListOfFiles) s
-  , views (asConfig . confFileBrowserView . fbSearchPathKeybindings) (renderKbGroup ManageFileBrowserSearchPath) s
+data KeybindingHelp =
+  KeybindingHelp
+    Text -- ^ keys
+    Text -- ^ description
+    String  -- ^ raw key event
+
+type HelpIndex = M.Map Name [KeybindingHelp]
+
+-- | Build a map between widget and it's configured key bindings
+--
+createKeybindingIndex :: InternalConfiguration -> HelpIndex
+createKeybindingIndex cfg = M.fromList
+  [ (ListOfThreads, views (confIndexView . ivBrowseThreadsKeybindings) kbGroup cfg)
+  , (ComposeSubject, views (confComposeView . cvSubjectKeybindings) kbGroup cfg)
+  , (ComposeBcc, views (confComposeView . cvBccKeybindings) kbGroup cfg)
+  , (ComposeCc, views (confComposeView . cvCcKeybindings) kbGroup cfg)
+  , (ComposeFrom, views (confComposeView . cvFromKeybindings) kbGroup cfg)
+  , (ComposeTo, views (confComposeView . cvToKeybindings) kbGroup cfg)
+  , (ConfirmDialog, views (confComposeView . cvConfirmKeybindings) kbGroup cfg)
+  , (SaveToDiskPathEditor, views (confMailView . mvSaveToDiskKeybindings) kbGroup cfg)
+  , (ScrollingMailViewFindWordEditor, views (confMailView . mvFindWordEditorKeybindings) kbGroup cfg)
+  , (MailAttachmentPipeToEditor, views (confMailView . mvPipeToKeybindings) kbGroup cfg)
+  , (MailAttachmentOpenWithEditor, views (confMailView . mvOpenWithKeybindings) kbGroup cfg)
+  , (MailListOfAttachments, views (confMailView . mvMailListOfAttachmentsKeybindings) kbGroup cfg)
+  , (ManageMailTagsEditor, views (confMailView . mvManageMailTagsKeybindings) kbGroup cfg)
+  , (SearchThreadsEditor, views (confIndexView . ivSearchThreadsKeybindings) kbGroup cfg)
+  , (ManageThreadTagsEditor, views (confIndexView . ivManageThreadTagsKeybindings) kbGroup cfg)
+  , (ScrollingMailView, views (confMailView . mvKeybindings) kbGroup cfg)
+  , (ScrollingHelpView, views (confHelpView . hvKeybindings) kbGroup cfg)
+  , (ComposeListOfAttachments, views (confComposeView . cvListOfAttachmentsKeybindings) kbGroup cfg)
+  , (ListOfFiles, views (confFileBrowserView . fbKeybindings) kbGroup cfg)
+  , (ManageFileBrowserSearchPath, views (confFileBrowserView . fbSearchPathKeybindings) kbGroup cfg)
   ]
 
-renderKbGroup :: Titleize a => a -> [Keybinding v ctx] -> Widget Name
-renderKbGroup name kbs =
-  withAttr helpTitleAttr (padBottom (Pad 1) $ txt (titleize name))
-  <=> padBottom (Pad 1) (vBox (renderKeybinding <$> uniqKBs))
+kbGroup :: [Keybinding v ctx] -> [KeybindingHelp]
+kbGroup kbs = fmap createKeybindingHelp uniqKBs
   where
     uniqKBs = nubBy ((==) `on` view kbEvent) kbs
 
-renderKeybinding :: Keybinding v ctx -> Widget Name
-renderKeybinding kb = let keys = view kbEvent kb
-                          actions = view (kbAction . aDescription) kb
-                      in withAttr helpKeybindingAttr (hLimit 30 (padRight Max $ txt $ ppKbEvent keys))
-                         <+> padLeft (Pad 3) (txt (intercalate " > " actions))
+createKeybindingHelp :: Keybinding v ctx -> KeybindingHelp
+createKeybindingHelp kb =
+  let keys = view kbEvent kb
+      actions = view (kbAction . aDescription) kb
+   in KeybindingHelp (ppKbEvent keys) (intercalate " > " actions) (show keys)
+
+renderHelp :: AppState -> Widget Name
+renderHelp s =
+  let index = createKeybindingIndex (view asConfig s)
+   in viewport ScrollingHelpView T.Vertical $
+      ifoldr renderKbGroup emptyWidget index
+
+renderKbGroup ::
+     Titleize a => a -> [KeybindingHelp] -> Widget Name -> Widget Name
+renderKbGroup name kbs sibling =
+  sibling
+  <=> withAttr helpTitleAttr (padBottom (Pad 1) $ txt (titleize name))
+  <=> padBottom (Pad 1) (vBox (renderKeybinding <$> kbs))
+
+renderKeybinding :: KeybindingHelp -> Widget Name
+renderKeybinding (KeybindingHelp keys actions _) =
+  withAttr helpKeybindingAttr (hLimit 30 (padRight Max $ txt keys)) <+>
+  padLeft (Pad 3) (txt actions)
 
 ppKbEvent :: Event -> Text
 ppKbEvent (EvKey k modifiers) = intercalate " + " $ (ppMod <$> modifiers) <> [ppKey k]
