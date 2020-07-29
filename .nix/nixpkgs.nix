31765255a931917d10d2146a44cc85c54b0f5b95
Róman Joost <roman@bromeco.de>
2020-07-29 15:02:16 +1000

    nix: use new release and GHC 8.8.4
    This pin's to a new nix release to avoid CI mostly fetching cached
    packages instead of rebuilding the (Nix) world.
    
    With bumping minimum versions for vty and brick, the minimum boundaries
    of all transitive packages has shifted upwards, causing a lot of
    derivations to be rebuilt due to the fact that they are not included in
    the package closure of the former pinned versions.

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,7 +1,7 @@
 { compiler ? null, nixpkgs ? null}:
 
 let
-  compilerVersion = if isNull compiler then "ghc865" else compiler;
+  compilerVersion = if isNull compiler then "ghc884" else compiler;
   haskellPackagesOverlay = self: super: with super.haskell.lib; {
     haskellPackages = super.haskell.packages.${compilerVersion}.override {
       overrides = hself: hsuper: {
@@ -21,11 +21,12 @@ let
   pkgSrc =
     if isNull nixpkgs
     then
+    # nixpkgs master - 2020-07-31
     builtins.fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/b9cb3b2fb2f45ac8f3a8f670c90739eb34207b0e.tar.gz";
-      sha256 = "1cpjmsa2lwfxg55ac02w9arbd3y5617d19x91sd1fq521jqbnnpc";
+      url = "https://github.com/NixOS/nixpkgs/archive/b5613e78fd8431af680a00c0cdd42a0637601c3a.tar.gz";
+      sha256 = "1fn8j4r7hx3bl1amdwmw9lyccjci891igrf9pz3vgkfq1bpmqd65";
     }
     else
     nixpkgs;
 in
-  import pkgSrc { overlays = [ haskellPackagesOverlay ]; }
+import pkgSrc { overlays = [ haskellPackagesOverlay ]; }
