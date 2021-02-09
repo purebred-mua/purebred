06b5f096c928c9c8432c0450a633085ca7c1c72e
Fraser Tweedale <frase@frase.id.au>
2021-02-09 20:55:58 +1000

    plugins: comprehensive documentation

--- src/Purebred/Plugin.hs
+++ src/Purebred/Plugin.hs
@@ -16,24 +16,146 @@
 
 {-|
 
-Types and helpers for plugins.
+The Purebred plugin system.
 
 -}
 module Purebred.Plugin
   (
   -- * Using plugins
+  -- $using
     usePlugin
   , PluginDict
 
-  -- * Constructing a plugin
+  -- ** Advanced usage for security-conscious users
+  -- $paranoia
+
+  -- * Defining plugins
+  -- $defining
   , Plugin(..)
 
+  -- ** Capabilities
+  -- $capabilities
+  , Pure, CanIO, CanReadConfig, CanRWAppState, Unconstrained
+
   -- ** Hooks
+  -- $hooks
   , PreSendHook(..)
   , ConfigHook(..)
-
-  -- ** Capabilities
-  , Pure, CanIO, CanReadConfig, CanRWAppState, Unconstrained
   ) where
 
 import Purebred.Plugin.Internal
+
+{- $using
+
+To use plugins, update your @~\/.config\/purebred\/purebred.hs@ to
+use the 'Purebred.purebredWithPlugins' entry point with a list of
+plugins.  Apply 'usePlugin' to each plugin value to prepare it for
+use.
+
+@
+main = 'Purebred.purebredWithPlugins'
+  [ 'usePlugin' ('Purebred.Plugin.TweakConfig.tweakConfig' tweak)
+  , 'usePlugin' Purebred.Plugin.ICU.plugin
+  ]
+@
+
+Some plugins could require additional configuration arguments.
+Refer to the documentation for individual plugin modules to find out
+how to configure them.
+
+Purebred plugins have the type @'Plugin' hooks@.  @hooks@ encodes
+the hooks implemented by the plugin, and the required capabilities.
+In this way, the plugin type is (machine-enforced) documentation
+about the possible range of plugin behaviour.
+
+-}
+
+{- $paranoia
+
+Security-conscious users can use type annotations to protect against
+plugins acquiring new capabilities upon upgrade.  In the example
+below, the ICU plugin is fixed at @ConfigHook Pure@.  If a new
+version changes to @ConfigHook CanIO@ or uses additional hooks, the
+program will not compile.  The user can decide whether to accept the
+new type (by updating the annotation), or remove the plugin.
+
+@
+main = 'Purebred.purebredWithPlugins'
+  [ 'usePlugin' (Purebred.Plugin.TweakConfig.tweakConfig tweak)
+  , 'usePlugin' (Purebred.Plugin.ICU.plugin :: 'Plugin' ('ConfigHook' 'Pure'))
+  ]
+@
+
+-}
+
+{- $defining
+
+The type of a plugin is @'Plugin' hooks@ where @hooks@ is either a
+single hook function newtype, or a nested tuple of hook functions.
+Many plugins will only use a single hook.  Some will use multiple
+hooks.
+
+To define a plugin, apply the 'Plugin' constructor to a plugin name,
+'Version', and hooks.  If the plugin requires user configuration (as
+in the following example), use function argument(s).
+
+@
+-- | Add a signature to outgoing messages
+signature :: Text -> Plugin (PreSendHook CanReadConfig)
+signature str = Plugin "UserAgent" version (PreSendHook hook)
+  where
+  hook msg = view confCharsets >>= manipulateTheMessage msg
+@
+
+Each hook has a set of /available/ capabilities, and plugins declare
+the /required/ capabilities for the implemented hooks.  Available
+capabilities differ and are documented at each hook newtype, and in
+the table below.
+
+-}
+
+{- $capabilities
+
+The plugin system defines the following capabilities.
+
++---------------------+--------------------------------+
+| Capabilitity        | Description                    |
+|                     |                                |
++=====================+================================+
+| 'Pure'              | The hook function can only     |
+|                     | perform a pure computation     |
+|                     | based on its input.            |
++---------------------+--------------------------------+
+| 'CanReadConfig'     | The hook function can read the |
+|                     | configuration via 'ask'.       |
++---------------------+--------------------------------+
+| 'CanRWAppState'     | The hook function can read and |
+|                     | write the 'AppState' via 'get' |
+|                     | and 'put'.                     |
++---------------------+--------------------------------+
+| 'CanIO'             | The hook function can perform  |
+|                     | I/O via 'liftIO'.              |
++---------------------+--------------------------------+
+| 'Unconstrained'     | Meta-capability that implies   |
+|                     | all defined capabilities.      |
++---------------------+--------------------------------+
+
+Not all capabilities are available to all hooks (see hooks table
+below).
+
+
+-}
+
+{- $hooks
+
++---------------------+----------------------------+------------------+
+| Hook                | Description                | Available        |
+|                     |                            | capabilities     |
++=====================+============================+==================+
+| 'ConfigHook'        | Modify config at startup   | 'CanIO'          |
++---------------------+----------------------------+------------------+
+| 'PreSendHook'       | Modify or process message  | 'Unconstrained'  |
+|                     | before sending             |                  |
++---------------------+----------------------------+------------------+
+
+-}
