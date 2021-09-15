65af01695a75447ea21bdd81b5562cf91e793ad7
Lucas Hoffmann <lucc@posteo.de>
2021-09-15 17:49:46 +0200

    Copy overlay code from .nix/nixpkgs.nix

--- flake.nix
+++ flake.nix
@@ -7,5 +7,31 @@
 
   outputs = { self, nixpkgs }: {
 
+    overlays = {
+      purebred = final: prev: with prev.haskell.lib; {
+        # TODO handle different compilers
+        haskellPackages = prev.haskell.packages.ghc884.override {
+          overrides = hself: hsuper: {
+            purebred = hsuper.callPackage .nix/purebred.nix { };
+            purebred-email = hsuper.callPackage .nix/purebred-email.nix { };
+            purebred-icu = hsuper.callPackage .nix/purebred-icu.nix { };
+            dyre = hsuper.callPackage .nix/dyre.nix { };
+            brick = hsuper.callPackage .nix/brick.nix { };
+          };
+        };
+      };
+    };
+
+    packages.x86_64-linux.purebred =
+      let pkgs = import nixpkgs { overlays = [ self.overlays.purebred ]; };
+      in pkgs.purebred;
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.purebred;
+
+#    devShell.x86_64-linux = nixpkgs.haskellPackages.shellFor {
+#      withHoogle = true;
+#      packages = haskellPackages: [ haskellPackages.purebred ] ++ icuPackageDep haskellPackages;
+#      nativeBuildInputs = haskellPackages.purebred.env.nativeBuildInputs ++ nativeBuildTools;
+#    };
+
   };
 }
