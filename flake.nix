98cf5e09b9e8d6d8adae286f897bf716acfb09d6
Lucas Hoffmann <lucc@posteo.de>
2021-09-15 11:47:28 +0200

    Initialize flake
    The lock file is initialized with the commit from .nix/nixpkgs.nix.

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "Purebred development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+  };
+}
