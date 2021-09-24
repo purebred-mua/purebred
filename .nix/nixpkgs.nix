8e18e35cf41b57e9dfaabb26c4a28ff49a3036c4
Lucas Hoffmann <lucc@posteo.de>
2021-09-24 20:23:26 +0200

    nix: load revision from flake.lock for legacy default.nix
    This loads the locked nixpkgs revision from flake.lock when evaluating
    default.nix with legacy tools like nix-build.  This ensures that the
    revision and hash are only kept in one place.

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -13,13 +13,14 @@ let
       };
     };
   };
+  lock = builtins.fromJSON (builtins.readFile ../flake.lock);
   pkgSrc =
     if isNull nixpkgs
     then
     # nixpkgs nixos-unstable - 2021-01-06
     builtins.fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/266dc8c3d052f549826ba246d06787a219533b8f.tar.gz";
-      sha256 = "09ydqx2lznixmw8z4cfz1j3k137mh8n3cdpygwqymknhfdjq7lg4";
+      url = "https://github.com/NixOS/nixpkgs/archive/${lock.nodes.nixpkgs.locked.rev}.tar.gz";
+      sha256 = lock.nodes.nixpkgs.locked.narHash;
     }
     else
     nixpkgs;
