d63ff5996a492d110980929631a988b52d1ab7f2
Róman Joost <roman@bromeco.de>
2021-10-26 18:24:51 +1000

    ci: update purebred-email nix derivation
    This should also fix NixOS CI

--- .nix/purebred-email.nix
+++ .nix/purebred-email.nix
@@ -1,26 +1,26 @@
 { mkDerivation, attoparsec, base, base64-bytestring, bytestring
 , case-insensitive, concise, deepseq, hedgehog, lens, lib
-, QuickCheck , quickcheck-instances, semigroupoids, semigroups
-, stringsearch, tasty, tasty-golden, tasty-hedgehog, tasty-hunit
-, tasty-quickcheck, text, time
+, QuickCheck, quickcheck-instances, random, semigroupoids
+, semigroups, stringsearch, tasty, tasty-golden, tasty-hedgehog
+, tasty-hunit, tasty-quickcheck, text, time
 }:
 mkDerivation {
   pname = "purebred-email";
-  version = "0.4.2";
-  sha256 = "7e611e912d7fbaa482eb059481f4074a4053bcda7bbfd9fb602476cbac8b92a1";
+  version = "0.5";
+  sha256 = "412dc455ff244f864b66ee00ad0fc5f9986cdbc41e60c62f7468108dddf47645";
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
     attoparsec base base64-bytestring bytestring case-insensitive
-    concise deepseq lens semigroupoids semigroups stringsearch text
-    time
+    concise deepseq lens random semigroupoids semigroups stringsearch
+    text time
   ];
   testHaskellDepends = [
     attoparsec base bytestring case-insensitive hedgehog lens
-    QuickCheck quickcheck-instances semigroups tasty tasty-golden
-    tasty-hedgehog tasty-hunit tasty-quickcheck text time
+    QuickCheck quickcheck-instances random semigroups tasty
+    tasty-golden tasty-hedgehog tasty-hunit tasty-quickcheck text time
   ];
   homepage = "https://github.com/purebred-mua/purebred-email";
   description = "types and parser for email messages (including MIME)";
-  license = lib.licenses.agpl3;
+  license = lib.licenses.agpl3Plus;
 }
