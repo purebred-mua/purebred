dd9ccab8759ab84af55f92f76d594f189804edcf
Róman Joost <roman@bromeco.de>
2022-07-03 16:37:27 +1000

    Refactor and simplify nix sources
    We currently define sources to build purebred in nix in different
    files. Both keep around redundant information found in both. Since nix
    flakes is still experimental, we didn't want to make `nix develop` the
    default when building the nix package. So we kept the older `nix-build`
    around which also defined overlays etc.
    
    Instead simplify everything by defining a single source of truth in
    `overlays` which is then used by two methods of building in nix: the old
    `nix-build` way and the newer `nix build`.

---
+++ .nix/overlays.nix
@@ -0,0 +1,55 @@
+let
+  haskellCompilerVersion = "ghc901";
+  haskellPackagesOverlay = self: super: with super.haskell.lib; {
+    haskellPackages = super.haskell.packages.${haskellCompilerVersion}.override {
+      overrides = hself: hsuper: {
+        purebred = hsuper.callPackage ./purebred.nix { };
+        purebred-email = hsuper.callPackage ./purebred-email.nix { };
+        purebred-icu = hsuper.callPackage ./purebred-icu.nix { };
+        dyre = hsuper.callPackage ./dyre.nix { };
+        brick = hsuper.callPackage ./brick.nix { };
+        typed-process = hsuper.callPackage ./typed-process.nix { };
+      };
+    };
+    make-purebred-with-packages = with-icu:
+    let
+      envPackages = self: if with-icu then [ self.purebred-icu ] else [ self.purebred ];
+      env = self.haskellPackages.ghcWithPackages envPackages;
+    in self.stdenv.mkDerivation {
+      name = "purebred-with-packages-${env.version}";
+      nativeBuildInputs = [ self.makeWrapper ];
+      # This creates a Bash script, which sets the GHC in order for dyre to be
+      # able to build the config file.
+      buildCommand = ''
+        mkdir -p $out/bin
+        makeWrapper ${env}/bin/purebred $out/bin/purebred \
+        --set NIX_GHC "${env}/bin/ghc"
+      '';
+      preferLocalBuild = true;
+      allowSubstitutes = false;
+    };
+    purebred-with-packages-icu = self.make-purebred-with-packages true;
+    purebred-with-packages = self.make-purebred-with-packages false;
+    make-purebred-shell = with-icu: let
+      nativeBuildTools = with self.haskellPackages; [
+        cabal-install
+        cabal2nix
+        ghcid
+        hlint
+        haskell-language-server
+        ormolu
+        hie-bios
+        self.notmuch
+        self.tmux
+        self.gnumake
+        self.asciidoctor
+        self.python3Packages.pygments
+      ];
+    in self.haskellPackages.shellFor {
+      withHoogle = true;
+      packages = hp: [ hp.purebred ] ++ (if with-icu then [hp.purebred-icu] else []);
+      nativeBuildInputs = self.haskellPackages.purebred.env.nativeBuildInputs ++ nativeBuildTools;
+    };
+  };
+
+in [haskellPackagesOverlay]
