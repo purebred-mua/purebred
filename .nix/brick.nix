8aafd0f265cb400cf52e269d4555b1791218064c
Róman Joost <roman@bromeco.de>
2023-11-21 11:23:30 +1000

    Upgrade dependencies too

--- .nix/brick.nix
+++ .nix/brick.nix
@@ -1,8 +1,8 @@
 { mkDerivation, base, bimap, bytestring, config-ini, containers
-, contravariant, data-clist, deepseq, directory, exceptions
-, filepath, lib, microlens, microlens-mtl, microlens-th, mtl
-, QuickCheck, stm, template-haskell, text, text-zipper, unix-compat
-, vector, vty, word-wrap
+, data-clist, deepseq, directory, exceptions, filepath, lib
+, microlens, microlens-mtl, microlens-th, mtl, QuickCheck, stm
+, template-haskell, text, text-zipper, unix-compat, vector, vty
+, vty-crossplatform, vty-unix, word-wrap
 }:
 mkDerivation {
   pname = "brick";
@@ -10,14 +10,15 @@ mkDerivation {
   sha256 = "30280d6f7130eb3e6cbf5a55465a06a825169cb536d3b2e91883aec23532b31e";
   isLibrary = true;
   isExecutable = true;
+  doCheck = false;
   libraryHaskellDepends = [
-    base bimap bytestring config-ini containers contravariant
-    data-clist deepseq directory exceptions filepath microlens
-    microlens-mtl microlens-th mtl stm template-haskell text
-    text-zipper unix-compat vector vty word-wrap
+    base bimap bytestring config-ini containers data-clist deepseq
+    directory exceptions filepath microlens microlens-mtl microlens-th
+    mtl stm template-haskell text text-zipper unix-compat vector vty
+    vty-crossplatform word-wrap
   ];
   testHaskellDepends = [
-    base containers microlens QuickCheck vector vty
+    base containers microlens QuickCheck vector vty vty-unix
   ];
   homepage = "https://github.com/jtdaugherty/brick/";
   description = "A declarative terminal user interface library";
